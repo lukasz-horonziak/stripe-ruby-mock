7b35ce8300bcdc0f6180d240a759a0930f8928a1
Łukasz <lukasz@horonziak.pl>
2019-08-13 15:37:25 +0200

    Add helper for automatic confirmation payment intent

--- lib/stripe_mock/test_strategies/mock.rb
+++ lib/stripe_mock/test_strategies/mock.rb
@@ -21,6 +21,27 @@ module StripeMock
           StripeMock.instance.upsert_stripe_object(object, attributes)
         end
       end
+
+      def automatic_confirm_payment_intent(client_secret)
+        pi_id = StripeMock.instance.payment_intents.find { |_k, v| v[:client_secret] == client_secret }[0]
+
+        if StripeMock.instance.payment_intents[pi_id][:status] == 'requires_confirmation' || StripeMock.instance.payment_intents[pi_id][:status] == 'requires_action'
+          charge = Stripe::Charge.create({
+            customer: StripeMock.instance.payment_intents[pi_id][:customer],
+            amount: StripeMock.instance.payment_intents[pi_id][:amount],
+            payment_method: StripeMock.instance.payment_intents[pi_id][:payment_method],
+            payment_intent: StripeMock.instance.payment_intents[pi_id][:id],
+            currency: StripeMock.instance.payment_intents[pi_id][:currency],
+            paid: true
+          })
+
+          StripeMock.instance.payment_intents[pi_id][:charges][:total_count] += 1
+          StripeMock.instance.payment_intents[pi_id][:charges][:data] << charge
+          StripeMock.instance.payment_intents[pi_id][:status] = 'succeeded'
+        end
+
+        StripeMock.instance.payment_intents[pi_id]
+      end
     end
   end
 end
